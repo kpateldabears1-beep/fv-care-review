e4529156447fa2089272c06db3ca402f4c9b1162
kpatels23 <kpatel23@tamu.edu>
2026-09-24 13:38:13 +0000

    Publish silhouette logo and accurate Louisiana map

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='fv-presentation-v5'
+const CACHE='fv-presentation-v6'
 const ROOT=new URL('./',self.location.href)
-const CORE=['./','fv-app-qr.png','fv-camera-fallback.mp4'].map(path=>new URL(path,ROOT).href)
+const CORE=['./','favicon.svg','fv-mark.svg','louisiana-census-2024.svg','fv-app-qr.png','fv-camera-fallback.mp4'].map(path=>new URL(path,ROOT).href)
 
 self.addEventListener('install',event=>event.waitUntil(
   caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()),
